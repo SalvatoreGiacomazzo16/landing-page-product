282370f86cd9bf1618613a8c421921e19400ded1
SalvatoreGiacomazzo16 <salvatore161giacomazzo@gmail.com>
2025-05-20 16:24:15 +0200

    reviews section complete with dynamic insertion of reviews and reviewers name with radio input

---
+++ script.js
@@ -0,0 +1,30 @@
+const reviews = [
+    "La paperella di gomma è diventata il mio strumento di debugging preferito. Spiegare i problemi al mio 'assistente' silenzioso mi ha aiutato a risolvere bug che altrimenti mi avrebbero fatto impazzire. È anche un ottimo antistress per le giornate difficili!",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis optio, fugiat consequuntur perspiciatis doloremque magni alias maxime, et architecto incidunt dolor, sequi sit. Facilis dolorem dicta illo deserunt laborum. Laudantium.",
+    "Voluptatum dicta id voluptatibus aliquam corrupti! In distinctio quaerat cum voluptatibus sed quisquam! Culpa, impedit earum numquam blanditiis nam, animi harum eum nulla soluta voluptatem similique accusamus eaque non enim.",
+    "Voluptatum dicta id voluptatibus aliquam corrupti! In distinctio quaerat cum voluptatibus sed quisquam! Culpa, impedit earum numquam blanditiis nam, animi harum eum nulla soluta voluptatem similique accusamus eaque non enim."
+
+]
+
+const reviewersNames = [
+    "David William - Feb 22, 2023",
+    "Arthur Lorem - Jan 16, 2024",
+    "Jacob Ipsum - Aug 26, 2022",
+    "Martha Dolor - March 8, 2021"
+]
+
+const radios = document.querySelectorAll('input[name="flexRadioDefault"]');
+const reviewText = document.getElementById("reviewText");
+const reviewerName = document.getElementById("reviewerName");
+
+radios.forEach(radio => {
+    radio.addEventListener('change', () => {
+        const index = parseInt(radio.value);
+        reviewText.textContent = reviews[index];
+        reviewerName.textContent = reviewersNames[index];
+    });
+});
+
+document.getElementById("radio1").checked = true;
+reviewText.textContent = reviews[0];
+reviewerName.textContent = reviewersNames[0];
